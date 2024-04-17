605c1acc9715f96d13a863807429b3305884b788
samaanhane98 <samaanhane@gmail.com>
2024-04-17 22:45:43 +0200

    Potentially fixed script

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::rc::Rc;
 use tokio_i3ipc::{reply::Workspace, I3};
 
 mod error;
@@ -7,8 +6,28 @@ use error::Result;
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     let mut i3 = I3::connect().await?;
+    let workspaces = i3.get_workspaces().await?;
 
-    let msg_body = format!("[title=\"^dropdown\"] scratchpad show; move absolute position 0 1 ppt; resize set height 35 ppt");
+    let active_workspace = workspaces
+        .clone()
+        .into_iter()
+        .find(|workspace| workspace.focused)
+        .ok_or("I3 error, no focussed workspace")?;
+
+    let Workspace { rect, .. } = active_workspace;
+
+    let mut msg_body = format!("[title=\"^dropdown\"] scratchpad show;");
+    i3.send_msg_body(tokio_i3ipc::msg::Msg::RunCommand, msg_body)
+        .await?;
+
+    msg_body = format!(
+        "[title=\"^dropdown\"] move absolute position {} 0 ppt",
+        rect.x
+    );
+    i3.send_msg_body(tokio_i3ipc::msg::Msg::RunCommand, msg_body)
+        .await?;
+
+    msg_body = format!("[title=\"^dropdown\"] resize set width 100 ppt");
     i3.send_msg_body(tokio_i3ipc::msg::Msg::RunCommand, msg_body)
         .await?;
 
